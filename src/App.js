b711ded2b84778e310353918dbf04e81efd9f2ee
ras1k <rasikabdullah@gmail.com>
2023-04-17 22:09:11 +0600

    api added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
 
   return (
     <div className="App">
-      <Counter></Counter>
+      <ExternalUsers></ExternalUsers>
     </div>
   );
+}
+
+function ExternalUsers() {
+  const [users, setUsers] = useState([]);
+  useEffect(() =>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUsers(data))
+  } , [])
+  return (
+    <div>
+      <h2>External Users</h2>
+      <p>{users.length}</p>
+      {
+        users.map(user => <User name={user.name} email={user.email}></User>)
+      }
+    </div>
+  )
+}
+
+function User(props){
+  return(
+    <div style={{
+      border: '2px solid blue',
+      margin: '10px',
+      borderRadius: '8px',
+      backgroundColor: 'aqua'
+    }}>
+      <h3>Name : {props.name}</h3>
+      <p>Email : {props.email}</p>
+    </div>
+  )
 }
 
 function Counter (){
